bd31a0fac6455e9380ab4de8bb5e9729acc05dc2
wanghao0000 <wanghao0000@997df810-cc65-11de-bd98-e341d93cc3cc>
2009-11-16 21:06:33 +0000

    Added start-wait feature.

--- trunk/src/com/ccmtc/android/droidguard/DroidGuardService.java
+++ trunk/src/com/ccmtc/android/droidguard/DroidGuardService.java
@@ -3,21 +3,30 @@
  */
 package com.ccmtc.android.droidguard;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import android.app.Service;
 import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.IBinder;
 import android.telephony.TelephonyManager;
 import android.util.Log;
+import android.widget.Toast;
 
 /**
+ * The main service of DroidGuard.
+ * 
  * @author Ken
  * 
  */
 public class DroidGuardService extends Service implements DetectorEventListener {
 
-	public static final String EXTRA_STOP_SERVICE = "com.ccmtc.android.droidguard.StopService";
+	// public static final String EXTRA_STOP_SERVICE =
+	// "com.ccmtc.android.droidguard.StopService";
+
 	private static final String logTag = "DroidGuardService";
 
 	private BroadcastReceiver sysBroadcastReceiver;
@@ -46,9 +55,6 @@ public class DroidGuardService extends Service implements DetectorEventListener 
 		initDetectors();
 		initNotifiers();
 		initSysBroadcastReceiver();
-
-		// Set status bar notification.
-		SysNotification.Set(this, SysNotification.NOTIFICATION_RUNNING);
 	}
 
 	/**
@@ -99,14 +105,64 @@ public class DroidGuardService extends Service implements DetectorEventListener 
 			return;
 		}
 		super.setForeground(true);
-		startAllDetectors();
-		this.registerReceiver(sysBroadcastReceiver, new IntentFilter(
-				"android.provider.Telephony.SMS_RECEIVED"));
-		this.registerReceiver(sysBroadcastReceiver, new IntentFilter(
-				TelephonyManager.ACTION_PHONE_STATE_CHANGED));
-		Log.d(logTag, "Service started.");
+
+		// Notify the user with a toast notification.
+		Toast.makeText(
+				this,
+				String.format(this.getText(
+						R.string.toast_service_about_to_start).toString(),
+						PrefStore.getStartGuardWaitSeconds(this)), 5).show();
+
+		// Wait a set time.
+		Timer startupTimer = new Timer();
+		startupTimer.schedule(new StartupTimerTask(this), PrefStore
+				.getStartGuardWaitSeconds(this) * 1000);
 	}
 
+	/**
+	 * The timer task to perform when startup and wait for a preparing time.
+	 * 
+	 * @author Ken
+	 */
+	private class StartupTimerTask extends TimerTask {
+
+		private final Context context;
+
+		/**
+		 * Create a new instance of {@link StartupTimerTask}.
+		 * 
+		 * @param context
+		 *            The context of the caller.
+		 */
+		public StartupTimerTask(Context context) {
+			this.context = context;
+		}
+
+		@Override
+		public void run() {
+			startAllDetectors();
+			registerSysBroadcasts(context);
+			// Set status bar notification.
+			SysNotification.Set(context, SysNotification.NOTIFICATION_RUNNING);
+			Log.d(logTag, "Service started.");
+		}
+	}
+
+	/**
+	 * 
+	 */
+	private void registerSysBroadcasts(Context context) {
+		context.registerReceiver(sysBroadcastReceiver, new IntentFilter(
+				"android.provider.Telephony.SMS_RECEIVED"));
+		context.registerReceiver(sysBroadcastReceiver, new IntentFilter(
+				TelephonyManager.ACTION_PHONE_STATE_CHANGED));
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see android.app.Service#onDestroy()
+	 */
 	@Override
 	public void onDestroy() {
 		super.onDestroy();
@@ -114,6 +170,7 @@ public class DroidGuardService extends Service implements DetectorEventListener 
 		stopAllNotifiers();
 		SysNotification.Unset(this, SysNotification.NOTIFICATION_RUNNING);
 		this.unregisterReceiver(sysBroadcastReceiver);
+		Toast.makeText(this, R.string.toast_service_stopped, 3).show();
 		Log.d(logTag, "Service stopped.");
 	}
 
